2e5a39d49129a7bc703cb971066ce0c55287c219
Hvmnn <felipe.hamen@alumnos.ucn.cl>
2025-01-09 16:05:07 -0300

    feat: add posts service.

---
+++ src/app/auth/services/posts.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostsService {
+
+  private apiUrl = 'http://localhost:5168/api/posts';
+
+  constructor(private http: HttpClient) { }
+
+  getPosts(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
+}
